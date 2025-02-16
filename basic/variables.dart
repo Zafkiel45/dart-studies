80b58ccdf3f06c320f24a1007f11cb2d688feb2c
zafkiel45 <zafkielc@gmail.com>
2025-02-16 10:23:50 -0300

    variables

---
+++ basic/variables.dart
@@ -0,0 +1,13 @@
+void main() {
+  // the type annotation is not mandatory, but I will specify in the real world
+  // Dart is a type-safer and dynamically, infere the correct type of "var"
+  var myName = "Murilo";
+  var myAge = 19;
+  var myWallet = 0.4;
+  var myFavoriteThings = ["Animes", "Games", "Programmer"];
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg',
+  };
+
+}
