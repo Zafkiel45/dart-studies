3c95b5ab5560de003cad897e66760c85762365f8
zafkiel45 <zafkielc@gmail.com>
2025-02-16 10:13:07 -0300

    hello world!

---
+++ basic/hello.dart
@@ -0,0 +1,6 @@
+void main() {
+  // the main function is necessary to execute the program
+  // this is like Java language. 
+  // The "main" function must not return anything. Because of this, your type is void.
+  print('Hello, World!');
+} 
