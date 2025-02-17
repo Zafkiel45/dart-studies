6a0e2e9f6e21ec13dcc6743e79ded9e6574246af
zafkiel45 <zafkielc@gmail.com>
2025-02-17 15:50:54 -0300

    enums(red flag)

---
+++ basic/enums.dart
@@ -0,0 +1,32 @@
+/*
+  I pratically do not know anything here! Enums are a mystery for me yet...
+  BUT, this will be my study object wit h red flag of preference. 
+
+  This file only exists to registratio proposes and visualizaiton.
+*/
+
+enum PlanetType { terrestrial, gas, ice }
+
+enum Planet {
+  mercury(planetType: PlanetType.terrestrial, moons: 0, hasRings: false),
+  venus(planetType: PlanetType.terrestrial, moons: 0, hasRings: false),
+  // ···
+  uranus(planetType: PlanetType.ice, moons: 27, hasRings: true),
+  neptune(planetType: PlanetType.ice, moons: 14, hasRings: true);
+
+  /// A constant generating constructor
+  const Planet({
+    required this.planetType,
+    required this.moons,
+    required this.hasRings,
+  });
+
+  /// All instance variables are final
+  final PlanetType planetType;
+  final int moons;
+  final bool hasRings;
+
+  /// Enhanced enums support getters and other methods
+  bool get isGiant =>
+      planetType == PlanetType.gas || planetType == PlanetType.ice;
+}
