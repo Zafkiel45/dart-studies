57130108a5a28f157a14f1e8fc2329913c2de282
zafkiel45 <zafkielc@gmail.com>
2025-02-17 15:31:34 -0300

    imports

---
+++ basic/imports.dart
@@ -0,0 +1,10 @@
+// here import from core librarie of Dart.
+import 'dart:math';
+// this import from packages. Something more complex and I will see this after
+// import 'package:test/test.dart';
+// below, the way of import another files. 
+import 'hello.dart';
+
+void main() {
+
+} 
