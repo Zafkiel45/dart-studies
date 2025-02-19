0934a727edff132099a76ba800eb551cfc26cdeb
zafkiel45 <zafkielc@gmail.com>
2025-02-19 14:18:02 -0300

    more built-in types

--- basic/built-in.dart
+++ basic/built-in.dart
@@ -161,4 +161,51 @@ void main() {
   var aBool = true;
   var aString = 'a string';
   const aConstList = [1, 2, 3];
+
+  // BOOLEANS ==================================================================
+  
+  // Booleans in Dart are the same than JavaScript, only "true" and "false".
+  // Even "true" and "false" are instance of class Boolean
+  // Dart doesn't allow to check non-boolean in conditionals or something what
+  // expect a boolean.
+
+  // Check for an empty string.
+  var fullName = '';
+  assert(fullName.isEmpty);
+
+  // Check for zero.
+  var hitPoints = 0;
+  assert(hitPoints == 0);
+
+  // Check for null.
+  var unicorn = null;
+  assert(unicorn == null);
+
+  // Check for NaN.
+  var iMeantToDoThis = 0 / 0;
+  assert(iMeantToDoThis.isNaN);
+
+  // Particularl, this is awesome! In JavaScript can be difficult predict the 
+  // result, but in Dart this is explicit! BUT, now it is necessary remember 
+  // all correct commands or ways to check!  
+
+  // RUNES =====================================================================
+
+  // Arrived the difficult part for me...! But, here we go. 
+  // Runes expose the Unicode code points perceived by the user.
+    // Strings are represents as code in memory, something like: \uXXXX
+    // Runes expose these code. 
+    // expose the Unicode code is useful in situations when it is necessary
+    // manipulate individual characters.
+  // in summary, there is a package to handle runes and characters. More 
+  // about this in another file. 
+
+  // Symbols ===================================================================
+
+  // Symbols are a unique identify, and in most of cases, it is useful when 
+  // creating APIs.
+
+  // Literal symbols:
+  #radix;
+  #bar;
 }
