321d0c5717a52ce4f1b90668f473f42c814df355
zafkiel45 <zafkielc@gmail.com>
2025-02-20 09:37:18 -0300

    main function

---
+++ basic/functions/main-function.dart
@@ -0,0 +1,9 @@
+void main(List<String> arguments) {
+    // 💎📁🏁🌟➡️
+    // ➡️ The main function is the entrypoint to the application 
+    // ➡️ The main function can have a optional List<string> as parameter 
+    // ➡️ The main function always return void
+    // ➡️ There is an official library to arguments in Dart
+
+    print(arguments); // prints an array of strings
+}
